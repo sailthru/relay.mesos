6efeee2ab223f402a660f0362c67f36381b333f6
Alex Gaudio <adgaudio@gmail.com>
2014-11-26 22:42:40 -0500

    simplify

--- relay_mesos/main.py
+++ relay_mesos/main.py
@@ -2,87 +2,95 @@
 Bootstrap the execution of Relay but first do the things necessary to setup
 Relay as a Mesos Framework
 """
-
+from greenlet import greenlet, getcurrent
 from relay import argparse_shared as at
 from relay.runner import main as relay_main, build_arg_parser as relay_ap
+from relay_mesos import log
 
 
-def mesos_offers_available():
+def wc_wrapper_factory(f):
     """
-    Return num mesos offers available
+    Wrap a warmer or cooler function such that, just before executing it, we
+    wait for mesos offers to ensure that the tasks can be created.
     """
-    raise NotImplementedError()
-    return 0
+    def warmer_cooler_wrapper(n):
+        # inform mesos that it should spin up n tasks of type f, where f is
+        # either the warmer or cooler.
+        # mutable['n_mesos_offers'] = mesosloop.switch(n, f)
+        # TODO n_mesos_offers is not necessary at the moment
 
+        # switch called from Relay
+        getcurrent().parent.switch(n, f)
+        n_fulfilled = getcurrent().parent.switch(n, f)
+        return n_fulfilled
 
-def base_wrapper(metric, target, warmer, cooler):
-    """
-    This function lets us peek at the PV and SP values before Relay does.
-    """
-    mutable = {}
-
-    def metric_wrapper():
-        """
-        set mutable['PV'] and before Relay gets to see it
-        """
-        gen = metric()
-        while True:
-            mutable['PV'] = next(gen)
-            yield mutable['PV']
-
-    def wc_wrapper_factory(f):
-        """
-        set mutable['MV'] to a (+) for warmer and (-) for cooler
-        before executing the warmer/cooler
-        """
-        def warmer_cooler_wrapper(n):
-            mutable['MV'] = n
-            return f(n)
+    if f is None:
+        return
+    else:
         return warmer_cooler_wrapper
-
-    def target_wrapper():
-        """If the mesos scheduler is at capacity, we trick Relay into
-        believing that there is nothing to do by artificially setting SP == PV.
-        When the scheduler receives offers again, Relay can resume.  We do
-        this to guarantee that errors due to resource contraints don't accrue
-        in Relay's history.  The assumption is that:
-            1) Mesos cluster inavailability has nothing to do with the metric
-            we're monitoring
-            2) maintining a repeated history of Mesos-induced error over time
-            will cause Relay to respond to noise and overreact
-        """
-        gen = target()
-        while True:
-            SP = next(gen)
-            n = mesos_offers_available()
-            MV = mutable['MV']
-            # TODO: THIS ASSUMES the metric is instantly updated, but metrics
-            # by definition are eventually consistent!  Maybe all this isn't
-            # going to pan out as well as I hope
-            if n < abs(MV):  # if less resources available than requested
-                # make the next SP artificially close to PV (error free)
-                unmet_requests = abs(MV) - n
-                yield mutable['PV'] + unmet_requests * (abs(MV) == MV)
-                # err = (SP - PV)
-            else:  # otherwise be normal
-                yield SP
-
-    return (
-        metric_wrapper, target_wrapper,
-        wc_wrapper_factory(warmer), wc_wrapper_factory(cooler)
-    )
 
 
 def main(ns):
-    ns.metric, ns.target, ns.warmer, ns.cooler = base_wrapper(
-        ns.metric, ns.target, ns.warmer, ns.cooler)
-    # TODO: mesos scheduler should *discard* any MV requests that it can't fill
-    return relay_main(ns)
+    # override warmer and cooler
+    ns.warmer = wc_wrapper_factory(ns.warmer)
+    ns.cooler = wc_wrapper_factory(ns.cooler)
+
+    mesosloop = greenlet(init_mesos_scheduler)
+    relayloop = greenlet(relay_main, parent=mesosloop)
+
+    mesosloop.switch(relayloop)  # start mesos framework
+
+    relayloop.switch(ns)  # start Relay. greenlets bounce control back and
+    # forth between mesos resourceOffers and Relay's warmer/cooler functions.
+
+
+def init_mesos_scheduler(relayloop):
+    # TODO: this function should start a proper mesos scheduler.  get rid of
+    # while loop and everything under it goes into the resourceOffers method.
+
+    # TODO: why does this approach not cause relay to freak out?  I think
+    # because relay blocks rather than build up an unnecessary error history
+    # that it would otherwise create if MVs didn't actually happen for some
+    # period of time.
+    log.info('Initializing Mesos Scheduler')
+    getcurrent().parent.switch()  # return context to main
+    n = 0  # debug
+
+    while True:  # assume this func is the resourceOffers that come in
+        n+=1  # debug
+        import random
+
+        navailable = int(random.choice([0]*2+ [3] * 3 + [10]))
+        # navailable = 15  # lets say I figured this out in resourceOffers
+
+        # wait on anything to use these available offers
+        # resume control in calling context (presumably relay)
+        log.warn(
+            'Mesos has offers available',
+            extra=dict(available_offers=navailable))
+
+        nrequests, func = relayloop.switch()
+        if n % 1000 > 500:
+            print 'skip'
+            continue
+        if func and navailable > 0:
+            n = min(navailable, nrequests)
+            func(n)
+            relayloop.switch(n)
+        else:
+            relayloop.switch(None)
+
+
+        log.warn(
+            'Relay requested some offers', extra=dict(
+                requested_offers=nrequests, func=func and func.func_name))
+        # if nrequests == 0:
+            # decline offers
+        # else:
+            # accept nrequests offers and start tasks
 
 
 build_arg_parser = at.build_arg_parser([
-    at.warmer(default='relay_mesos.warmer'),
-    at.cooler(default='relay_mesos.cooler'),
     at.group(
         "Relay.Mesos specific parameters",
         at.add_argument('--abc')),
