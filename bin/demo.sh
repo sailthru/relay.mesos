fd9089c43d4a29fcf5521f5732519ebc3089d1ab
Alex Gaudio <adgaudio@gmail.com>
2014-12-04 21:16:40 -0500

    use breerly/mesos docker mesos image because its nice

--- bin/demo.sh
+++ bin/demo.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# This script runs zookeeper and mesos inside docker containers.
+# It then runs relay.mesos as a mesos framework inside a docker container.
+
+
+# To see relay.mesos in action, navidate your browser to:
+#     http://docker:8080  # relay.mesos
+#     http://docker:5050  # mesos
+
 # install boot2docker
 # https://docs.docker.com/installation/mac/
 # ** don't forget to add env vars to your .profile **
@@ -11,46 +19,90 @@
 
 dir="$( cd "$( dirname "$( dirname "$0" )" )" && pwd )"
 
-# (dangerously) remove previous containers in case they exist
-docker rm -f relay.mesos
-if [ "$2" = "go" ] ; then
-  docker rm -f zookeeper
-  docker rm -f mesos_master
+if [ "${1:-0}" != "0" ] ; then
+  # remove previous containers in case they exist
+  docker rm -f $(docker ps -a |grep relay.mesos|awk '{print $NF}')
+
+  set -e
+  set -u
+
+  echo start zookeeper
+  docker run -d --name relay.mesos__zookeeper -d \
+    -p 2181:2181 \
+    -p 2888:2888 \
+    -p 3888:3888 \
+    jplock/zookeeper:3.4.6
+
+
+  echo start mesos master
+  docker run -d --name relay.mesos__mesos_master \
+    --link relay.mesos__zookeeper:zookeeper \
+    -e MESOS_CLUSTER=relay.mesos__DEMO \
+    -e MESOS_HOSTNAME=localdocker \
+    -e MESOS_WORK_DIR=/var/lib/mesos \
+    -e MESOS_LOG_DIR=/var/log \
+    -e MESOS_QUORUM=1 \
+    -e MESOS_ZK=zk://zookeeper:2181/mesos \
+    -p 5050:5050 \
+    breerly/mesos \
+    mesos-master
+    # -v /tmp/mesoswork:/tmp/mesoswork \
+    # -v /var/run/docker.sock:/var/run/docker.sock \
+    # -v /sys:/sys \
+    # -v /proc:/proc \
+    # -t -i \
+
+
+  echo start mesos slave  # MESOS_CONTAINERIZERS=mesos,docker is broken because the /run/docker.sock is incorrect
   for n in `seq 1 ${1:-1}` ; do
-    docker rm -f mesos_slave_${n}
-  done
-
-  # start zookeeper
-  docker run -d --name zookeeper -d -p 2181:2181 -p 2888:2888 -p 3888:3888 jplock/zookeeper:3.4.6
-
-  # start mesos master
-  docker run -d --name mesos_master --link zookeeper:ZK -e MESOS_LOG_DIR=/var/log -e MESOS_WORK_DIR=/tmp/mesoswork -e MESOS_QUORUM=1 -p 5050:5050 -t -i redjack/mesos-master bash -c 'ZK_MESOS=$ZK_PORT_2181_TCP_ADDR:$ZK_PORT_2181_TCP_PORT/ mesos-master'
-
-  # start mesos slave
-  for n in `seq 1 ${1:-1}` ; do
-    # docker run --name mesos_slave_$n -v $dir/relay_mesos:/mnt/relay_mesos --link zookeeper:ZK --link mesos_master:MASTER -e MESOS_LOG_DIR=/var/log -e MESOS_WORK_DIR=/tmp/mesoswork -t -i redjack/mesos-slave bash -c 'ZK_MESOS=$ZK_PORT_2181_TCP_ADDR:$ZK_PORT_2181_TCP_PORT/ MESOS_MASTER=$MASTER_PORT_5050_TCP_ADDR:$MASTER_PORT_5050_TCP_PORT/ mesos-slave'
-    # TODO: dont use a hacked docker image. use containers
-    # for now, hack use a docker build that is the relay.mesos build + mesos slave build + slaves support docker containers
-    docker run -d --name mesos_slave_$n -v $dir/relay_mesos:/mnt/relay_mesos --link zookeeper:ZK --link mesos_master:MASTER -e MESOS_LOG_DIR=/var/log -e MESOS_WORK_DIR=/tmp/mesoswork -t -i mesos_slave_11 bash -c 'ZK_MESOS=$ZK_PORT_2181_TCP_ADDR:$ZK_PORT_2181_TCP_PORT/ MESOS_MASTER=$MASTER_PORT_5050_TCP_ADDR:$MASTER_PORT_5050_TCP_PORT/ mesos-slave  -–containerizers=docker,mesos'
+    docker run -d --name relay.mesos__mesos_slave_$n \
+      --link relay.mesos__zookeeper:zookeeper \
+      --privileged=true \
+      -e MESOS_CONTAINERIZERS=docker,mesos \
+      -e MESOS_HOSTNAME=localdocker \
+      -e MESOS_MASTER=zk://zookeeper:2181/mesos \
+      -v /tmp/mesoswork:/tmp/mesoswork \
+      -v /var/run/docker.sock:/var/run/docker.sock \
+      -v /sys:/sys \
+      -v /proc:/proc \
+      -p 505$n:505$n \
+      -v $dir/relay_mesos:/mnt/relay_mesos \
+      breerly/mesos \
+      supervisord -n
+      # --privileged \
+      # --net=host \
+      # -e MESOS_WORK_DIR=/tmp/mesoswork \
+      # -e MESOS_LOG_DIR=/var/log \
+      # -e MESOS_EXECUTOR_REGISTRATION_TIMEOUT=5mins \
+      # -e MESOS_ISOLATOR=cgroups/cpu,cgroups/mem \
   done
 fi
 
+
 docker build -t relay.mesos .
 
-docker run -d --name relay.mesos --link mesos_master:MASTER -t -i relay.mesos bash -c 'MESOS_MASTER=$MASTER_PORT_5050_TCP_ADDR:$MASTER_PORT_5050_TCP_PORT/ relay.mesos -w bash_echo_warmer -m bash_echo_metric -d .1 --sendstats tcp://192.168.59.3:5498 -t 60 --task_resources cpus=1 mem=10 --lookback 300'
-
-( (sleep 10 ;echo relay.mesos demo quiting ; docker rm -f relay.mesos) & )
+docker run -d --name relay.mesos \
+  --link relay.mesos__zookeeper:zookeeper \
+  -t -i relay.mesos \
+  bash -c \
+  'relay.mesos -w bash_echo_warmer'\
+' -m bash_echo_metric '\
+' -d .1 --sendstats tcp://192.168.59.3:5498'\
+' -t 60 --task_resources cpus=1 mem=10 --lookback 300'\
+' --mesos_master zk://zookeeper:2181/mesos'
 
 # kill this after a little while
+( (sleep 10 ;echo relay.mesos demo quiting ; docker rm -f relay.mesos) & )
+
 docker logs -f relay.mesos
 
 
 # helpful
 # docker ps
-# docker kill $(docker ps -a -q)
-# docker restart zookeeper
-# docker restart mesos_master
-# docker restart mesos_slave_1
-# docker rm -f zookeeper
-# docker rm -f mesos_master
-# docker rm -f mesos_slave_1
+# docker kill $(docker ps -a |grep relay.mesos|awk '{print $1}')
+# docker restart relay.mesos__zookeeper
+# docker restart relay.mesos__mesos_master
+# docker restart relay.mesos__mesos_slave_1
+# docker rm -f relay.mesos__zookeeper
+# docker rm -f relay.mesos__mesos_master
+# docker rm -f relay.mesos__mesos_slave_1
