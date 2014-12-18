9b50d258a1476a13033f6539559e6b4723944000
Alex Gaudio <adgaudio@gmail.com>
2014-12-18 17:33:46 -0500

    Back to development: 0.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='relay.mesos',
-    version='0.0.4',
+    version='0.0.5.dev0',
     description=(
         'A plugin for Relay that lets you run Relay on Mesos to'
         ' auto-scale the number of concurrently running instances'
