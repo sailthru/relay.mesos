b8be2b89f425ab8106a6b6140e8cdef97c3a8269
Alex Gaudio <adgaudio@gmail.com>
2015-04-15 00:48:04 -0400

    tweak logging such that relay.runner and relay.mesos logs are by default both handled by a parent log named 'relay'

--- relay_mesos/__init__.py
+++ relay_mesos/__init__.py
@@ -3,7 +3,12 @@ log = logging.getLogger('relay.mesos')
 
 # expose configure_logging to those who wish to develop relay
 from relay import configure_logging
-configure_logging(True, log=log)
+
+from relay import log as _relay_runner_log
+_parent_log = logging.getLogger('relay')
+_relay_runner_log.propagate = True
+log.propagate = True
+configure_logging(True, log=_parent_log)  # parent_log will not propagate
 
 import pkg_resources as _pkg_resources
 __version__ = _pkg_resources.get_distribution('relay.mesos').version
