e278d3adaf6c4876686c4ff9376010681037010a
Max Sperlich <msperlich@sailthru.com>
2016-05-13 15:58:09 -0400

    First pass on removing relay as a depdency
    This version removes relay, and works directly with stolos to get the
    number of jobs to run.

--- relay_mesos/main.py
+++ relay_mesos/main.py
@@ -6,8 +6,8 @@ import signal
 import sys
 import time
 
-from relay import argparse_shared as at
-from relay.runner import main as relay_main, build_arg_parser as relay_ap
+import argparse_tools as at
+from argparse_tools import add_argument_default_from_env_factory
 from relay_mesos import log
 from relay_mesos.util import catch
 from relay_mesos.scheduler import Scheduler
@@ -16,33 +16,6 @@ from mesos.interface import mesos_pb2
 
 class TimeoutError(Exception):
     pass
-
-
-def warmer_cooler_wrapper(MV, ns):
-    """
-    Act as a warmer or cooler function such that, instead of executing code,
-    we ask mesos to execute it.
-    """
-    def _warmer_cooler_wrapper(n):
-        # inform mesos that it should spin up n tasks of type f, where f is
-        # either the warmer or cooler.  Since Relay assumes that the choice of
-        # `f` (either a warmer or cooler func) is determined by the sign of n,
-        # we can too!
-        log.debug(
-            'asking mesos to spawn tasks',
-            extra=dict(
-                mesos_framework_name=ns.mesos_framework_name,
-                task_num=n, task_type="warmer" if n > 0 else "cooler"))
-        t = time.time()
-        with MV.get_lock():
-            if MV[1] < t:
-                MV[:] = (n, t)
-        log.debug(
-            '...finished asking mesos to spawn tasks',
-            extra=dict(
-                mesos_framework_name=ns.mesos_framework_name,
-                task_num=n, task_type="warmer" if n > 0 else "cooler"))
-    return _warmer_cooler_wrapper
 
 
 def set_signals(relay, ns):
@@ -109,10 +82,6 @@ def main(ns):
 
     # copy and then override warmer and cooler
     ns_relay = ns.__class__(**{k: v for k, v in ns.__dict__.items()})
-    if ns.warmer:
-        ns_relay.warmer = warmer_cooler_wrapper(MV, ns)
-    if ns.cooler:
-        ns_relay.cooler = warmer_cooler_wrapper(MV, ns)
 
     mesos_scheduler, mesos_driver = init_mesos_driver(ns=ns,
                                      MV=MV,
@@ -126,7 +95,7 @@ def main(ns):
     relay_name = "Relay.Runner Event Loop"
     relay = mp.Process(
         target=catch(init_relay, exception_sender),
-        args=(ns_relay, relay_ready, ns.mesos_framework_name),
+        args=(ns_relay, relay_ready, MV),
         name=relay_name)
     mesos.daemon = True
     mesos.start()  # start mesos framework
@@ -167,25 +136,37 @@ def main(ns):
         if num_jobs != 0:
             log.debug("Number of jobs to run %d non-zero, reviving offers" % num_jobs)
             mesos_scheduler.revive_offers()
-        else:
-            log.debug("No work to perform")
 
         # save cpu cycles by checking for subprocess failures less often
-        if ns.delay > 5:
-            time.sleep(5)
-        else:
-            time.sleep(ns.delay)
+        log.debug("Delay is %s" % ns.delay)
+        time.sleep(min(5, ns.delay))
 
     relay.terminate()
     sys.exit(1)
 
 
-def init_relay(ns_relay, relay_ready, mesos_framework_name):
-    log.debug(
-        'Getting ready to start relay process',
-        extra=dict(mesos_framework_name=mesos_framework_name))
+def init_relay(ns_relay, relay_ready, MV):
+    import os
+    import stolos.api
+    app_name = os.environ['APP_NAME']
+    stolos.api.initialize([])
+    q = stolos.api.get_qbclient().LockingQueue(app_name)
+    log.debug('Getting ready to start relay process')
     relay_ready.set()
-    relay_main(ns_relay)
+    while True:
+        qsize = q.size(queued=True, taken=True)
+        # excluding the running tasks is expensive, so only do it if we know
+        # that the number is relatively small. If the number is large, we'll
+        # just return the total qsize and hope for the best
+        if qsize < 5000:
+            qsize = q.size(queued=True, taken=False)
+        log.debug("got stolos queue size: %s" % qsize)  # temp testing
+        t = time.time()
+        with MV.get_lock():
+            if MV[1] < t:
+                MV[:] = (-qsize, t)
+        time.sleep(min(5, ns_relay.delay))
+
 
 
 def init_mesos_driver(ns, MV, exception_sender, mesos_ready):
@@ -236,9 +217,10 @@ def init_mesos_scheduler(driver):
 # This add_argument func will prefix env vars with RELAY_MESOS.
 # The normal at.add_argument func prefixes env vars with RELAY_
 # Let's use the at.add_argument func for --mesos_XXX and the below for --XXX
-add_argument = at.add_argument_default_from_env_factory(
+add_argument = add_argument_default_from_env_factory(
     env_prefix='RELAY_MESOS_')
 
+relay_add_argument = add_argument_default_from_env_factory(env_prefix='RELAY_')
 
 def argparse_dict_type(inpt_str):
     return dict(
@@ -250,16 +232,7 @@ def argparse_tuple_kv_type(input_str):
 
 
 build_arg_parser = at.build_arg_parser([
-    at.group(
-        "How does Relay.mesos affect your metric?",
-        at.warmer(type=str, help=(
-            "A bash command to run on a mesos slave."
-            " A warmer should eventually increase metric values.")),
-        at.cooler(type=str, help=(
-            "A bash command to run on a mesos slave."
-            " A cooler should eventually decrease metric values.")),
-    ),
-    at.group(
+   at.group(
         "Relay.Mesos -- General parameters",
         add_argument(
             '--init_timeout', default=20, help=(
@@ -270,30 +243,30 @@ build_arg_parser = at.build_arg_parser([
     ),
     at.group(
         "Relay.Mesos -- Mesos-specific parameters",
-        at.add_argument(
+        relay_add_argument(
             '--mesos_master',
             help="URI to mesos master. We support whatever mesos supports"
         ),
-        at.add_argument(
+        relay_add_argument(
             '--mesos_framework_principal', type=str, help=(
                 "If you use Mesos Framework Rate Limiting, this framework's"
                 " principal identifies which rate limiting policy to apply")),
-        at.add_argument(
+        relay_add_argument(
             '--mesos_framework_role', type=str, help=(
                 "If you use Mesos Access Control Lists (ACLs) or apply"
                 " weighting to frameworks, your framework needs to register"
                 " with a role.")),
-        at.add_argument(
+        relay_add_argument(
             '--mesos_framework_name',
             default='framework',
             help="Name the framework so you can identify it in the Mesos UI"),
-        at.add_argument(
+        relay_add_argument(
             '--mesos_checkpoint', action='store_true', type=bool, default=False,
             help=(
                 "This option enables Mesos Framework checkpointing.  This"
                 " means that tasks spun up by Relay.Mesos will survive even if"
                 " this Relay.Mesos instance dies.")),
-        at.add_argument(
+        relay_add_argument(
             '--mesos_task_resources', type=argparse_dict_type,
             default={}, help=(
                 "Specify what resources your task needs to execute.  These"
@@ -301,7 +274,7 @@ build_arg_parser = at.build_arg_parser([
                 " as a comma separated list.  ie:"
                 "  --mesos_task_resources cpus=10,mem=30000"
             )),
-        at.add_argument(
+        relay_add_argument(
             '--mesos_environment', type=lambda fp: [
                 tuple(y.strip() for y in x.strip().split('=', 1))
                 for x in open(fp).readlines()],
@@ -310,7 +283,7 @@ build_arg_parser = at.build_arg_parser([
                 " warmer and cooler tasks."
                 "File should contain one variable per line, in form VAR1=VAL1"
             )),
-        at.add_argument(
+        relay_add_argument(
             '--mesos_attribute_matches_all', type=argparse_dict_type,
             help=(
                 "Only spin up tasks on mesos slaves whose attributes match the"
@@ -346,6 +319,10 @@ build_arg_parser = at.build_arg_parser([
                 "The name of a docker image if you wish to execute the"
                 " warmer and cooler in it")),
         add_argument(
+            '-d', '--delay', type=float, default=1,
+                    help='num seconds to wait between metric polling. ie. 1/sample_rate'
+                ),
+        add_argument(
             '--docker_network', choices=("HOST", "BRIDGE", "NONE"),
             default="BRIDGE", help=(
                 "Docker: Set the Network mode for the container: --net ")),
@@ -366,12 +343,11 @@ build_arg_parser = at.build_arg_parser([
                 " following format:"
                 "  --mesos_volumes host_path:container_path:mode,"
                 "host_path2:container_path2:mode,...")),
-    ),
+    )
 ],
     description=(
         "Use Relay to auto-scale instances of a bash command"
-        " or docker container on Mesos"),
-    parents=[relay_ap()], conflict_handler='resolve')
+        " or docker container on Mesos"))
 
 
 if __name__ == '__main__':
